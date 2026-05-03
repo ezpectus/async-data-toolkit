4570296df257d03d042ee46997b9b0af49c3cf8c
Denis Stepanenko <stepanenko.den2006@outlook.com>
2026-05-03 18:44:00 +0100

    cleanup event logging output

--- async-data-toolkit/src/events.js
+++ async-data-toolkit/src/events.js
@@ -2,14 +2,10 @@ import EventEmitter from "events";
 
 export class MessageSystem extends EventEmitter {
     sendMessage(message) {
-        console.log("Sending message:", message);
-
         this.emit("message", message);
     }
 
     sendNotification(notification) {
-        console.log("Notification:", notification);
-
         this.emit("notification", notification);
     }
 }
