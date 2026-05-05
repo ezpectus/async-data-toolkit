5cf86991764f2e488ee6ae2c312be0fb68f77f0d
Denis Stepanenko <stepanenko.den2006@outlook.com>
2026-05-05 18:47:00 +0100

    add proxy rate limiting support

--- async-data-toolkit/src/authProxy.js
+++ async-data-toolkit/src/authProxy.js
@@ -1,9 +1,18 @@
 export class AuthProxy {
-    constructor(apiKey) {
+    constructor(apiKey, limit = 5) {
         this.apiKey = apiKey;
+
+        this.limit = limit;
+        this.requestCount = 0;
     }
 
     async request(url) {
+        if (this.requestCount >= this.limit) {
+            throw new Error("Rate limit exceeded");
+        }
+
+        this.requestCount++;
+
         console.log("Sending request to:", url);
 
         const response = await fetch(url, {
